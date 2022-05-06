ec189987b9bcd69c38c046824c45a1081203e0e5
Comrade-sama <64259842+Comrade-sama@users.noreply.github.com>
2022-05-07 00:21:45 +1000

    Getting articles by following category of user function was added

--- YesterdayTimesApi/Controllers/UserController.cs
+++ YesterdayTimesApi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using YesterdayTimesApi.Entities;
 
 namespace YesterdayTimesApi.Controllers
 {
+    [Authorize]
     [Route("user")]
     [ApiController]
     public class UserController : ControllerBase
@@ -18,17 +20,49 @@ namespace YesterdayTimesApi.Controllers
         {
             this.repository = repository;
         }
-        [HttpPost("create")]
+        [AllowAnonymous]
+        [HttpPost("singup")]
         public async Task<ActionResult> RegisterUserAsync(CreatedUser created)
         {
+            List<Category> selectedCategories = new()
+            {
+                await repository.GetCategoryAsync(created.SelctedCategoryId1),
+                await repository.GetCategoryAsync(created.SelctedCategoryId2),
+                await repository.GetCategoryAsync(created.SelctedCategoryId3)
+            };
+
             User user = new()
             {
                 Id = Guid.NewGuid(),
                 Email = created.Email,
-                Password = created.Password
+                Password = created.Password,
+                Categories = selectedCategories                
             };
             await repository.RegistrateUserAsync(user);
             return NoContent();
+        }
+        [AllowAnonymous]
+        [HttpGet("users")]
+        public async Task<IEnumerable<UserDetailedDTO>> GetAsync()
+        {
+            var users = (await repository.GetUsersAsync()).Select(user => user.UserAsDetailedDTO());
+            return users;
+        }
+        [AllowAnonymous]
+        [HttpGet("articles")]
+        public async Task<IEnumerable<ArticleDTO>> GetUsersArticlesAsync( Guid id)
+        {
+            var user = await repository.GetUserAsync(id);
+            List<ArticleDTO> articles = new();
+            foreach(Category category in user.Categories)
+            {
+                var _category = await repository.GetCategoryAsync(category.Id);
+                foreach (Article article in _category.Articles)
+                {
+                    articles.Add(article.ArticleAsDTO());
+                }
+            }
+            return articles;
         }
     }
 }
